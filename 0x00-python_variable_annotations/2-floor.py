ba20b0b58f85e7b24171ef040baee8ee2504dbc8
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-10-06 02:03:24 -0700

    type annotation

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""A function that calculates the floor of a float."""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Calculate the floor of a float and return the result as an integer.
+
+    Args:
+        n (float): The input float.
+
+    Returns:
+        int: The floor of the input float.
+    """
+    return math.floor(n)
