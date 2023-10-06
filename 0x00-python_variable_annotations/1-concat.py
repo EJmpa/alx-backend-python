24e675b899c19239c28fc41449bd89044fef7552
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-10-06 01:57:39 -0700

    type annotation

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""A function that concatenates two strings."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenate two strings and return the result.
+
+    Args:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Returns:
+        str: The concatenated string.
+    """
+    return str1 + str2
