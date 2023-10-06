4e2749d2a510ba977fdba1a9878bd3fe2422159b
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-10-06 02:09:27 -0700

    type annotation

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""A function that converts a float to its string representation."""
+
+
+def to_str(n: float) -> str:
+    """
+    Convert a float to its string representation.
+
+    Args:
+        n (float): The input float.
+
+    Returns:
+        str: The string representation of the input float.
+    """
+    return str(n)
