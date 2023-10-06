6f466260c9545d0de2920d2dc779ee6e4b0bc92a
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-10-06 01:50:58 -0700

    type annotation

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""A function that add two float and return float"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Add two floats and return their sum.
+
+    Args:
+        a (float): The first float to add.
+        b (float): The second float to add.
+
+    Returns:
+        float: The sum of a and b as a float.
+    """
+    return a + b
