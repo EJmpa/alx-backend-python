1ce88d15436cd3eea6f5174d966875272c08d2cb
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-10-06 05:41:50 -0700

    type annotation

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Type Annotation"""
+
+from typing import Tuple, List
+
+
+def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
+    """
+    Zoom in on a tuple by repeating each element a specified number of times.
+
+    Args:
+        lst (Tuple[int, ...]): A tuple of integers to be zoomed in on.
+        factor (int, optional): The zoom factor. Each element in the tuple
+        will be repeated
+            'factor' times. Defaults to 2.
+
+    Returns:
+        List[int]: A list containing the zoomed-in elements.
+    """
+    zoomed_in: List[int] = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
---
+++ 0x00-python_variable_annotations/test.py
@@ -0,0 +1,13 @@
+def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
+    zoomed_in: Tuple = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3.0)
